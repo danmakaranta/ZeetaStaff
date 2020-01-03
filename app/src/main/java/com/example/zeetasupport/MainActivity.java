0625b386192906ac0f8b7a0b28e56f6b55e7f10d
faisalsash <faisalsash@yahoo.com>
2020-01-03 23:24:24 +0100

    all permissions added to manifest and getPermissionstatus created

--- app/src/main/java/com/example/zeetasupport/MainActivity.java
+++ app/src/main/java/com/example/zeetasupport/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zeetasupport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -24,12 +25,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(isServicesOk()){
+        if (isServicesOk()) {
             init();
         }
     }
 
-    private void init(){
+    private void getLocationPermission() {
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET};
+        
+    }
+
+    private void init() {
         Button btnMap = (Button) findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,25 +47,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public boolean isServicesOk(){
-        Log.d(TAG,"isServicesOk: checking google services version");
+    public boolean isServicesOk() {
+        Log.d(TAG, "isServicesOk: checking google services version");
 
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
 
-        if(available == ConnectionResult.SUCCESS){
+        if (available == ConnectionResult.SUCCESS) {
             //everything is ok and the user can make a map request
             Log.d(TAG, "isServicesOk: Google play services is working");
             return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             //an error occured but we can resolve it
             Log.d(TAG, "isServicesOk: an error occured but we can fix it");
 
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available, ERROR_DIALOG_REQUEST);
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }
-        else{
-            Toast.makeText(this, "You can't make map request",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "You can't make map request", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
